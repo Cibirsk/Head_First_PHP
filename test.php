bbf43019e9814579fe827918e6bbb426859b3a97
Cibirsk <stlv@gmx.com>
2021-11-16 10:35:58 +0100

    none

--- test.php
+++ test.php
@@ -1,4 +1,9 @@
 <?php
-    echo "test";
-    phpinfo();
+
+    //$yourFirstName= $_POST['myfirstname'];
+    $yourName= $_POST['myname'];
+    //$yourTown= $_POST['mytown'];
+    //echo 'your firstname is: ' . $yourFirstName;
+    echo 'your name is: ' . $yourName; 
+    //echo 'you live in: ' . $yourTown;
 ?>
