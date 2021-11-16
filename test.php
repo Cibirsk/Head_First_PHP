5ba7df9c531036227ed63b924b2be24422d009d6
Cibirsk <stlv@gmx.com>
2021-11-16 17:40:17 +0100

    none

--- test.php
+++ test.php
@@ -2,7 +2,9 @@
 
     $yourFirstName= $_POST['firstname'];
     $yourName= $_POST['name'];
+    $yourTown= $_POST['town'];
     echo 'your firstname is: ' . $yourFirstName . '<br />';
-    echo 'your name is: ' . $yourName; 
+    echo 'your name is: ' . $yourName . '<br />';
+    echo 'you live in: ' . $yourTown;
 
 ?>
