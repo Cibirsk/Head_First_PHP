2654522d879ac5e8938e52b49e0c9ccfcc216755
Cibirsk <stlv@gmx.com>
2021-11-17 10:03:17 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,21 @@
 <?php
 
+/*
     $yourFirstName= $_POST['firstname'];
     $yourName= $_POST['name'];
     $yourTown= $_POST['town'];
     echo 'your firstname is: ' . $yourFirstName . '<br />';
     echo 'your name is: ' . $yourName . '<br />';
     echo 'you live in: ' . $yourTown;
+*/
+    $first='one';
+    $two='two';
+    $three='three';
+    $four= 2+2;
+    echo 'test 1' . '<br>';
+    echo 'test 2 ' . '<br />';
+    echo $first . '<br>' . $two . '<br>' . $three . '<br>' . $four;
+
+    
 
 ?>
