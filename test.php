dfab064f28813d39e8b2db47cbbe262bc61d34a6
Cibirsk <stlv@gmx.com>
2021-11-17 18:13:44 +0100

    Update test.php

--- test.php
+++ test.php
@@ -8,13 +8,12 @@
     echo 'your name is: ' . $yourName . '<br />';
     echo 'you live in: ' . $yourTown;
 */
-    $first='one';
-    $two='two';
-    $three='three';
+    $first="one";
+    $two="two";
+    $three="three";
     $four= 2+2;
-    echo 'test 1' . '<br>';
-    echo 'test 2 ' . '<br />';
-    echo $first . '<br>' . $two . '<br>' . $three . '<br>' . $four;
+    echo " I saw $first bird and $two cats <br>
+    but $three dogs";
 
     
 
