614408e0057e8f0a269d8b884c8c91b4f6f4439a
Cibirsk <stlv@gmx.com>
2021-12-08 18:19:29 +0100

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -18,9 +18,22 @@
 
     $query = "SELECT * FROM email_list" ;
 
-    $result = mysqli_query($dbc, $query) or die('erreur dans la requête');
+    $result = mysqli_query($dbc, $query) 
+                 or die('erreur dans la requête');
 
-    $row = mysqli_fetch_array($result);
+    while($row = mysqli_fetch_array($result)){
+        $first_name = $row['first_name'];
+        $last_name = $row['last_name'];
+
+        $msg = "Dear $first_name $last_name, \n $text";
+        $to = $row['email'];
+
+        mail($to,$subject,$msg, 'from: ' . $from);
+
+        echo 'Mail sent to: ' . $to . '<br>';
+    }
+    
+    mysqli_close($dbc);
 
 ?>
 </body>
