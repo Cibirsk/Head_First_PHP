9b9b3a828b113bcca85770a22a2cc8ab303fc88a
Cibirsk <stlv@gmx.com>
2021-12-14 17:40:57 +0100

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -9,47 +9,51 @@
 <body>
 <?php    
 
-    $from = 'stlv@gmx.com';
-    $subject = $_POST['subject'];
-    $text= $_POST['elvismail'];
-    $ouput_form = false;
+    if(isset($_POST['submit'])){
+        $from = 'stlv@gmx.com';
+        $subject = $_POST['subject'];
+        $text= $_POST['elvismail'];
+        $ouput_form = false;
 
-    if(empty($subject) && empty($text)){
-        echo 'Subject and text are empty';
+        if(empty($subject) && empty($text)){
+            echo 'Subject and text are empty';
+            $ouput_form = true;
+        }
+
+        if(empty($subject) && !empty($text)){
+            echo 'Subject are empty';
+            $ouput_form = true;
+        }
+
+        if(!empty($subject) && empty($text)){
+            echo 'Text are empty';
+            $ouput_form = true;
+        }
+
+        if(!empty($subject) && !empty($text)){
+
+            $dbc = mysqli_connect('localhost' , 'root' , 'root' , 'elvis_store')
+                    or die('erreur de connnexion à la base');
+
+            $query = "SELECT * FROM email_list" ;
+
+            $result = mysqli_query($dbc, $query) 
+                        or die('erreur dans la requête');
+
+            while($row = mysqli_fetch_array($result)){
+                $first_name = $row['first_name'];
+                $last_name = $row['last_name'];
+
+                $msg = "Dear $first_name $last_name, \n $text";
+                $to = $row['email'];
+
+                mail($to,$subject,$msg, 'from: ' . $from);
+
+                echo 'Mail sent to: ' . $to . '<br>';
+            }    
+        }
+    }else{
         $ouput_form = true;
-    }
-
-    if(empty($subject) && !empty($text)){
-        echo 'Subject are empty';
-        $ouput_form = true;
-    }
-
-    if(!empty($subject) && empty($text)){
-        echo 'Text are empty';
-        $ouput_form = true;
-    }
-
-    if(!empty($subject) && !empty($text)){
-
-        $dbc = mysqli_connect('localhost' , 'root' , 'root' , 'elvis_store')
-                or die('erreur de connnexion à la base');
-
-        $query = "SELECT * FROM email_list" ;
-
-        $result = mysqli_query($dbc, $query) 
-                    or die('erreur dans la requête');
-
-        while($row = mysqli_fetch_array($result)){
-            $first_name = $row['first_name'];
-            $last_name = $row['last_name'];
-
-            $msg = "Dear $first_name $last_name, \n $text";
-            $to = $row['email'];
-
-            mail($to,$subject,$msg, 'from: ' . $from);
-
-            echo 'Mail sent to: ' . $to . '<br>';
-        }    
     }
 
     if($ouput_form){
