6a19f3382c44d51cf7216ac30d83fb7e478d7bf6
Cibirsk <stlv@gmx.com>
2021-12-18 13:28:52 +0100

    none

--- removeemail.php
+++ removeemail.php
@@ -1,29 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Make Me Elvis - Remove Email</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
+
+  <p>Please select the email addresses to delete from the email list and click Remove.</p>
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+
 <?php
-
-    $email = $_POST['email'];
-
     $dbc = mysqli_connect('localhost' , 'root' , 'root' , 'elvis_store')
             or die('erreur de connnexion à la base');
 
-    $query = "DELETE FROM email_list WHERE email = '$email' ";
-    
-    $result = mysqli_query($dbc,$query)
-                or die('erreur dans la requête');
+  // Delete the customer rows (only if the form has been submitted)
+  if (isset($_POST['submit'])) {
+    foreach ($_POST['todelete'] as $delete_id) {
+      $query = "DELETE FROM email_list WHERE id = $delete_id";
+      mysqli_query($dbc, $query)
+        or die('Error querying database.');
+    } 
 
+    echo 'Customer(s) removed.<br />';
+  }
 
-    mysqli_close($dbc);
+  // Display the customer rows with checkboxes for deleting
+  $query = "SELECT * FROM email_list";
+  $result = mysqli_query($dbc, $query);
+  while ($row = mysqli_fetch_array($result)) {
+    echo '<input type="checkbox" value=" ' . $row['id'] . ' "  name="todelete[]" />';
+    echo $row['first_name'];
+    echo ' ' . $row['last_name'];
+    echo ' ' . $row['email'];
+    echo '<br />';
+  }
 
-    echo 'Your email ' . $email . ' has been removed';
-
+  mysqli_close($dbc);
 ?>
+
+    <input type="submit" name="submit" value="Remove" />
+  </form>
 </body>
 </html>
